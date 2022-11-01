7a6cc66c6be9d4a62a6a5f67e09a63f365c4e864
Taiwolawal <taiwolawal360@gmail.com>
2022-11-01 12:01:34 +0100

    added edited values for pro and network

--- s3.tf
+++ s3.tf
@@ -32,9 +32,9 @@ resource "aws_s3_bucket_acl" "pro_acl" {
 
 
 
-# block public access :
-resource "aws_s3_bucket_public_access_block" "public_block" {
-  bucket = aws_s3_bucket.bucket.id
+# block public access to pro and network bucket :
+resource "aws_s3_bucket_public_access_block" "public_block_network" {
+  bucket = aws_s3_bucket.network.id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -42,9 +42,19 @@ resource "aws_s3_bucket_public_access_block" "public_block" {
   ignore_public_acls      = true
 }
 
-# encrypt bucket using SSE-S3:
+resource "aws_s3_bucket_public_access_block" "public_block_pro" {
+  bucket = aws_s3_bucket.pro.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
+
+
+# encrypt bucket using SSE-S3 to pro and network bucket :
 resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt" {
-  bucket = aws_s3_bucket.bucket.id
+  bucket = aws_s3_bucket.network.id
 
   rule {
     apply_server_side_encryption_by_default {
@@ -53,9 +63,19 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt" {
   }
 }
 
-# create S3 website hosting:
+resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt" {
+  bucket = aws_s3_bucket.pro.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# create S3 website hosting for pro and network:
 resource "aws_s3_bucket_website_configuration" "website" {
-  bucket = aws_s3_bucket.bucket.id
+  bucket = aws_s3_bucket.bucknet.worker.id
   index_document {
     suffix = "index.html"
   }
@@ -63,6 +83,18 @@ resource "aws_s3_bucket_website_configuration" "website" {
     key = "error.html"
   }
 }
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.bucknet.pro.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "error.html"
+  }
+}
+
+
 
 # add bucket policy to let the CloudFront OAI get objects:
 resource "aws_s3_bucket_policy" "bucket_policy" {
